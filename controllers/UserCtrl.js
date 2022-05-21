63f2a6b14b13b37911e2482b3c2fbedd2fd0e8ea
Sagat <sagatfarhat@gmail.com>
2022-05-22 02:28:09 +0600

    first commit

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -3,44 +3,7 @@ const md5 = require("md5");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 
-/*
 exports.register = async (req, res) => {
-    bcrypt.hash(req.body.password, 18, function(err, hash) {
-        const newUser = new UserModel({
-            email: req.body.username,
-            password:hash
-        });
-        newUser.save(function (err) {
-            if (err) {
-                res.status("404").render("404");
-            } else {
-                res.render("secretPage",{username:req.body.username,statusOf:"Registered"});
-            }
-        });
-    })
-};
-
-exports.login = async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    UserModel.findOne({email: username}, function(err, foundUser){
-        if (err) {
-            res.status("404").render("404");
-        } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result===true) {
-                        res.status("200").render("secretPage",{username:req.body.username,statusOf:"Logged in"});
-                    }
-                });
-            }
-        }
-    })
-};
- */
-
-exports.register = async (req, res) => {
-    //level 5
     UserModel.register({username: req.body.username}, req.body.password, function (err, user) {
         if (err){
             console.log(err)
@@ -54,7 +17,6 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    //level 5
     let user =new UserModel({
         username:req.body.username,
         password:req.body.password
